f760a426e746ff3d3baef85d595687c160f0d35b
Alvarezzzzz <alejandrojalvarezg.2005@gmail.com>
2024-07-01 17:36:22 -0400

    Se agrego que se pudiesen seleccionar los asientos junto con sus validaciones

--- P3/asientos_seleccion.js
+++ P3/asientos_seleccion.js
@@ -1,58 +1,76 @@
-document.addEventListener('DOMContentLoaded', function() {
+/* RECORDATORIOS
+contains(class) --> verifica si el elemento tiene esa clase, si la tiene devuelv true sino false
+e.target
+*/
+
+//Funcion que suma la cantidad de pasajeros
+function sumarPasajeros() {
     const adultsInput = document.getElementById('adults');
     const childrenInput = document.getElementById('children');
     const infantsInput = document.getElementById('infants');
-    const seats = document.querySelectorAll(".seat");
-    const selectedSeatInput = document.getElementById("seats");
+    const adultos = parseInt(adultsInput.value) || 0;
+    const niños = parseInt(childrenInput.value) || 0;
+    const infantes = parseInt(infantsInput.value) || 0;
+    
+    return adultos + niños + infantes;
+}
 
-    function sumarPasajeros() {
-        const adultos = parseInt(adultsInput.value) || 0;
-        const niños = parseInt(childrenInput.value) || 0;
-        const infantes = parseInt(infantsInput.value) || 0;
-        
-        return adultos + niños + infantes;
-    }
 
-    function actualizarDisponibilidadAsientos() {
-        const totalPasajeros = sumarPasajeros();
-        
-        if (totalPasajeros > 0) {
-            seats.forEach(asiento => {
-                const asientoNumero = parseInt(asiento.dataset.value.substring(1));
-                
-                if (asientoNumero <= totalPasajeros) {
-                    asiento.disabled = false;
-                } else {
-                    asiento.disabled = true;
-                }
-            });
-        } else {
-            // Si no hay pasajeros, deshabilitar todos los asientos
-            seats.forEach(asiento => {
-                asiento.disabled = true;
-            });
+var asientosHTML = document.querySelectorAll(".seat")
+var inputAsientos = document.getElementById("seats")
+
+//Obtenemos los inputs de la cantidad de pasajeros
+const adultsInput = document.getElementById('adults');
+const childrenInput = document.getElementById('children');
+const infantsInput = document.getElementById('infants');
+//Los almacenamos en una lista
+var todosLosInputs = [adultsInput , childrenInput , infantsInput]
+
+//agregamos una funcion en los inputs para que cada vez que se modifique, se deseleccionen los asientos
+todosLosInputs.forEach((input)=>{
+    input.addEventListener("input", ()=>{
+        inputAsientos.value = ""
+        asientosHTML.forEach((asiento) =>{
+            asiento.classList.remove("seleccionado")
+        })
+    })
+})
+
+asientosHTML.forEach(asiento => {
+    asiento.addEventListener("click" , function(e) {
+
+        //Obtenemos el asiento que desencadeno el evento
+        var asientoDelEvento = e.target
+
+        //verificamos si el asiento ya esta seleccionado para deseleccionarlo
+        if (asientoDelEvento.classList.contains("seleccionado") ) {
+            asientoDelEvento.classList.remove("seleccionado");
+            //Quitamos el asiento del input
+            inputAsientos.value = inputAsientos.value.replace(asientoDelEvento.getAttribute("data-value"), "")
+            return null
         }
-    }
 
-    seats.forEach(seat => {
-        seat.addEventListener("click", function() {
-            seat.classList.toggle("seat-selected");
-            selectedSeatInput.value = seat.getAttribute("data-value");
-        });
-    });
+        //verifica si el asiento esta ocupado, para que no haga nada
+        if (asientoDelEvento.classList.contains("ocupado")) {
+            return null
+        }
 
-    adultsInput.addEventListener('input', function() {
-        actualizarDisponibilidadAsientos();
-    });
+        //si el asiento esta libre, procedemos a hacer validaciones
+        let contAsientos= sumarPasajeros();
+        let contAsientosSelect = 0;
 
-    childrenInput.addEventListener('input', function() {
-        actualizarDisponibilidadAsientos();
-    });
+        //contamos la cantidad de asientos seleccionados
+        asientosHTML.forEach(asiento =>{
+            if (asiento.classList.contains("seleccionado")) {
+                contAsientosSelect += 1;
+            }
+        })
+        //verificamos que la cantidad de los asientos seleccionados, no pase que la cantidad proporcionada por el usuario
+        if (contAsientos > contAsientosSelect) {
+            inputAsientos.value += " " + asientoDelEvento.getAttribute("data-value")
+            asientoDelEvento.classList.add("seleccionado")
+        }
+    })
+})
 
-    infantsInput.addEventListener('input', function() {
-        actualizarDisponibilidadAsientos();
-    });
 
-    // Llamar a la función inicial para establecer la disponibilidad inicial de asientos
-    actualizarDisponibilidadAsientos();
-});
